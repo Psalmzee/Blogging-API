dd3bd876f534ac5352bff95dd95a255d087515a9
Samson Okeji <engrsamsonokeji@gmail.com>
2022-10-28 16:46:25 +0100

    Added starter files and folders

---
+++ app.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const app = require('./index')
+const Database = require('./database');
+
+const PORT = process.env.PORT || 3334
+
+// connect to database
+Database.connect();
+
+app.listen(PORT, () => {
+    console.log('Listening on port, ', PORT)
+})
